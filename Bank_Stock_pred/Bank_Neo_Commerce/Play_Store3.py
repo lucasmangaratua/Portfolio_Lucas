6f04b41a889d4cf426f49f63e47f4c4e39123f3c
Lucas Mangaratua <112455652+lucasmangaratua@users.noreply.github.com>
2022-08-30 18:19:15 +0700

    added neo python

---
+++ Bank_Stock_pred/Bank_Neo_Commerce/Play_Store3.py
@@ -0,0 +1,16 @@
+from google_play_scraper import app
+import pandas as pd
+import numpy as np
+
+from google_play_scraper import Sort, reviews_all
+reviews = reviews_all(
+    'com.bnc.finance',
+    sleep_milliseconds=0, 
+    lang=‘id’, 
+    country=‘id’, 
+    sort=Sort.NEWEST, 
+)
+
+df = pd.DataFrame(np.array(reviews),columns=['review'])
+df = df.join(pd.DataFrame(df.pop('review').tolist()))
+df.head()
